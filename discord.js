1c477243838a117118e39b58041c1380ef41a86b
joeysapp <sapp.joey@gmail.com>
2023-04-18 07:51:48 -0500

    [v0.1.8] Making logging a little better

--- discord.js
+++ discord.js
@@ -1,48 +1,42 @@
+// VERY MESSY RIGHT NOW :^)
+// https://discordjs.guide/
+// https://discord.com/developers/applications/
+
+// https://old.discordjs.dev/#/docs/discord.js/14.9.0/
+// https://old.discordjs.dev/#/docs/discord.js/14.9.0/class/Activity
+// https://old.discordjs.dev/#/docs/discord.js/14.9.0/class/Presence
+// https://old.discordjs.dev/#/docs/discord.js/14.9.0/class/User
+
+// https://discordjs.guide/popular-topics/faq.html#how-do-i-check-the-bot-s-ping
+
 // todo(@joeysapp):
 //    -> what do I want this thing to even do lol
 //       e.g. fun stuff vs huge overbuilt/engineering thing.
+
 // ## Review Site/PG structure for best practices (e.g. classes, etc.)
 // - Function wrappers for Users, Presences, Events, Interactions 
-//   - Comparison, printing, etc.
+//   - Comparison, printing, etc?
 // - Store necessary information (PG?)
+
 // NOTE: toString of a lot of things hide certain fields.
-// Check here: https://old.discordjs.dev/#/docs/discord.js/14.9.0/class/User
 
-//  _______ __   __ __
-// |   |   |  |_|__|  |
-// |   |   |   _|  |  |
-// |_______|____|__|__|
-function Message(message) { }
-function Interaction(interaction) { }
-function Guild(guild) { }
-function Presence(presence) { }
-function Activity(activity) { }
-function User(user) {
-  this.userID = null;
+// const sleep = require('node:timers/promises').setTimeout;
 
-  function constructor(user) {
-    // lol { this } = ...user;
-  }
+function Status(status) {
+  this.desktop = status.desktop ? status.desktop : '';
+  this.mobile =  status.mobile ? status.mobile : '';
 
-  function toString() {
-    /* Library for indent/pretty printing */
-  }
+}
+Status.prototype.toString = function foobar() {
+  return `desktop=${this.desktop}\tmobile=${this.mobile}`;
 }
 
-
-
-// Helpful links:
-// https://discordjs.guide/
-// https://old.discordjs.dev/#/docs/discord.js/14.9.0/
-// https://discord.com/developers/applications/
-
-const { clientSecret, applicationID } = require('./env.json'); // Env vars instead
-
-const sleep = require('node:timers/promises').setTimeout;
 const { numToStr } = require('./utils/fmt.js');
 const { genSeed, genI32 } = require('./lib/twister.js'); // Pseudo RNG for /roll
+const { clientSecret, applicationID } = require('./env.json'); // Env vars instead
 
-// Bot 'client' creation
+// ================================================================================
+// We're initializing our bot here
 const {
   Client,
   Events,
@@ -63,83 +57,30 @@ const bennoBot = new Client({
     GatewayIntentBits.DirectMessages,
     GatewayIntentBits.DirectMessageReactions,
     GatewayIntentBits.DirectMessageTyping,
-    GatewayIntentBits.MessageContent, // messages.read, needs to be authed
+    GatewayIntentBits.MessageContent,
   ],
 });
-
+let selfID = null;
+let selfUsername = null;
+let selfAvatar = null;
 bennoBot.once(Events.ClientReady, function(currentClient) {
   console.log(`Events.ClientReady: ${currentClient.user.tag} is now listening for interactions`);
+  selfID = currentClient.user.id;
+  selfUsername = currentClient.user.username;
+  selfAvatar = currentClient.user.avatar;
   genSeed(new Date().getTime());
 });
 
-// https://discord.com/developers/docs/topics/gateway#presence-update
-// https://old.discordjs.dev/#/docs/discord.js/14.9.0/class/Client?scrollTo=e-presenceUpdate
-bennoBot.on(Events.PresenceUpdate, async function(oldPres, newPres) {
-  // note(@joeysapp): I think we need to establish a wss connection for this? or not?
 
-  // Messy, just figuring stuff out atm
-  const { userId: userID } = oldPres || newPres;
-  let { status: oldStatus, activities: oldActivities, clientStatus: oldClientStatus } = oldPres || {};
-  let { status: newStatus, activities: newActivities, clientStatus: newClientStatus } = newPres || {};
-  console.log(`Events.PresenceUpdate({ userID: ${userID} })`);
-  
-  if (oldActivities && oldActivities.length) {
-    const { name, details, state } = oldActivities[0];
-    oldActivities = `[${name}] ${details} - ${state}`;
-  }
-  if (newActivities && newActivities.length) {
-    const { name, details, state } = newActivities[0];
-    newActivities = `[${name}] ${details} - ${state}`;
-  }
-  oldClientStatusString = '';
-  Object.keys(oldClientStatus || {}).sort().forEach(function(k, idx) {
-    oldClientStatusString += `${idx > 0 ? ', ' : ''}${k}: ${oldClientStatus[k]}`;
-  });
-  newClientStatusString = '';
-  Object.keys(newClientStatus || {}).sort().forEach(function(k, idx) {
-    newClientStatusString += `${idx > 0 ? ', ' : ''}${k}: ${newClientStatus[k]}`;
-  });
 
-  if (oldClientStatusString !== newClientStatusString) {
-    console.log(` - ${oldClientStatusString}`);
-    console.log(` > ${newClientStatusString}`);
-  }
-  if ((oldActivities && oldActivities.length || newActivities && newActivities.length) && (oldActivities !== newActivities)) {
-    console.log(` - ${oldActivities}`);
-    console.log(` > ${newActivities}`);
-  }
-});
-
-bennoBot.on(Events.GuildMemberUpdate, async function(oldMember, newMember) {
-  console.log('Events.GuildMemberUpdate', oldMember, newMember);
-});
-
-bennoBot.on(Events.MessageUpdate, async function(oldMsg, newMsg) {
-  const { createdTimestamp, content, author: { username, id, }, attachments, stickers, mentions, content: oldContent } = oldMsg;
-  const { content: newContent, editedTimestamp } = newMsg;
-  const ts = new Date(editedTimestamp).toLocaleString();
-  console.log(`Events.MessageUpdate({ @${username} at ${ts} })`);
-  console.log(` - "${oldContent}"`);
-  console.log(` > "${newContent}"`);
-});
-
-bennoBot.on(Events.MessageCreate, async function(msg) {
-  const { createdTimestamp, content, author: { username, id: userID, }, attachments, stickers, mentions } = msg;
-  if (userID === applicationID) return;
-  const ts = new Date(createdTimestamp).toLocaleString();
-
-  console.log(`Events.MessageCreate({ @${username} at ${ts}})`);
-  console.log(` + "${content}"`);
-});
-
+// ================================================================================
+// This handles all "slash commands" to this bot
 bennoBot.on(Events.InteractionCreate, async function(interaction) {
   const {
     commandName,
     id,
-    user: {
-      username,
-      id: userID,
-    },
+    user: { username, id: userID },
+    createdAt,
   } = interaction;
 
   let cmdString = `/${commandName} `;
@@ -147,35 +88,40 @@ bennoBot.on(Events.InteractionCreate, async function(interaction) {
     const { name, value, type } = opt;
     cmdString += `${name}=${value} `;
   });
-  console.log(`Events.InteractionCreate({ id: ${id} })`);
+  const ts = new Date(createdTimestamp);
+
+  console.log(`[${ts.toLocaleTimeString()}] Events.InteractionCreate({ @${username} })`);
   console.log(` + ${cmdString}`);
-  console.log(` - @${username} [${userID}]`);
   
-  const responses = [];
-  let wasDeferred = false;
-  if (interaction.commandName === 'roll') {
-    //  ______         __ __
-    // |   __ \.-----.|  |  |
-    // |      <|  _  ||  |  |
-    // |___|__||_____||__|__|
+  const responses = []; // All sent in sequence for now? vs. just concat?
+  let wasDeferred = false; // For future network/db reqs
+  let ephemeral = false;
+
+
+  // roll _sides _seed
+  // slap @user
+  // played
+  // weather _zip
+  // activity -> last activity on the server XD
+
+  if (interaction.commandName === 'slap') {
+    
+  } else if (interaction.commandName === 'weather') {
+
+  } else if (interaction.commandName === 'activity') {
+
+  } else if (interaction.commandName === 'roll') {
     const sides = interaction.options.getInteger('sides');
     const seed = interaction.options.getInteger('seed');
     if (sides <= 0) {
-      wasDeferred = true;
-      await interaction.deferReply({ ephemeral: false });
-      await sleep(1000);
-      await interaction.editReply(`Okay, dude, a die with ${sides} sides isn't possible. Are you high?`);
-      return;
+      responses.push(`Hey man. A "rollable" die with ${sides} sides isn't possible. Are you high?`);
+    } else {
+      genSeed(seed || (new Date()).getTime());
+      const result = ((genI32()) % sides) + 1;
+      const seedString = seed ? `seeded with ${seed} ` : '';
+      responses.push(`A ${sides}-sided die ${seedString}rolled a ${result}.`);
     }
-    genSeed(seed || (new Date()).getTime());
-    const result = (genI32()) % sides;
-    const seedString = seed ? `seeded with ${seed} ` : '';
-    responses.push(`A ${sides}-sided die ${seedString}rolled a ${result}.`);
-  } else if (interaction.commandName === 'info') {
-    //  _______         ___
-    // |_     _|.-----.'  _|.-----.
-    //  _|   |_ |     |   _||  _  |
-    // |_______||__|__|__|  |_____|
+  } else if (interaction.commandName === 'played') {
     let unit = 'day';
     if (genI32() % 2 === 0) {
       unit = 'year';
@@ -205,7 +151,10 @@ bennoBot.on(Events.InteractionCreate, async function(interaction) {
     let msg = '';
     if (guildTenure < accountAge * 0.25) {
       if (rareString) rareString = `I'll ask Yuan to take care of you. `;
-      msg += `Please stop bothering me, ${interaction.user.username}. ${rareString}`;
+      let snarkyComment = '';
+      snarkyComment = 'Please stop bothering me';
+
+      msg += `${snarkyComment}, ${interaction.user.username}. ${rareString}`;
       msg += `Your account is ${numToStr(accountAge)} ${unit}s old and you've only been here ${numToStr(guildTenure)} ${unit}s. `;
       msg += `That means you've been here only ${percentageInGuild}% of your time on Discord. Please work on your ratio.`;
     } else {
@@ -217,32 +166,98 @@ bennoBot.on(Events.InteractionCreate, async function(interaction) {
     // responses.push(`Please stop bothering me, ${interaction.user.username}. You've only been in this Discord for ${guildTenure} days.`);
   }
 
+  // lol, if somehow we didn't generate any string.
+  if (!responses[0]) responses.push('wat'); 
+
+  console.log(`[${ts.toLocaleTimeString()}] Events.MessageCreate({ @${username} })`);
   let idx = 0;
-  // note(@joeysapp): I think the editReply/defer stuff should only be used for actual network requests,
-  //  - same with interaction.followUp
   if (wasDeferred) {
+    // todo(@joeysapp): This will be used with loading/network commands, e.g. weather or rswiki #@%!
+    // Also, interaction.followUp('yeet');
     await interaction.editReply(responses[idx])
       .then(function(resInt) {
-        console.log(`Events.InteractionCreate({ id: ${id} })`);
         console.log(` -> "${responses[idx]}"`);
       });
   } else {
-    await interaction.reply(responses[idx])
+    await interaction.reply({ content: responses[idx], ephemeral })
       .then(async function(finishedInteraction) {
-        console.log(`Events.InteractionCreate({ id: ${id} })`);
         console.log(` -> "${responses[idx]}"`);
-        // if (responses.length > 1) {
-        //   await interaction.followUp(responses[1]);
-        // }
       });
   }
 });
 
+
+
+// ================================================================================
+// This tells the bot how to interpret any given message (but not "slash commands"!)
+bennoBot.on(Events.MessageCreate, async function(msg) {
+  const { createdTimestamp, content, author: { username, id: userID, }, attachments, stickers, mentions } = msg;
+  if (userID === applicationID) return;
+  const ts = new Date(createdTimestamp);
+
+  // Did this message contain a User mention, and was it aimed at us?
+
+  console.log(`[${ts.toLocaleTimeString()}] Events.MessageCreate({ @${username} })`);
+  console.log(` + "${content}"`);
+});
+
+bennoBot.on(Events.MessageUpdate, async function(oldMsg, newMsg) {
+  const { createdTimestamp, content, author: { username, id, }, attachments, stickers, mentions, content: oldContent } = oldMsg;
+  const { content: newContent, editedTimestamp } = newMsg;
+  const ts = new Date(editedTimestamp);
+  console.log(`[${ts.toLocaleTimeString()}] Events.MessageUpdate({ @${username} })`);
+  console.log(` - "${oldContent}"`);
+  console.log(` > "${newContent}"`);
+});
+
+
+// ================================================================================
+// This watches/handles all Server member status/activity updates
+// https://discord.com/developers/docs/topics/gateway#presence-update
+bennoBot.on(Events.PresenceUpdate, async function(oldPres, newPres) {
+  const { userId: userID, user: { username } } = oldPres || newPres;
+  let { status: oldStatus, activities: oldActivities, clientStatus: oldClientStatus } = oldPres || {};
+  let { status: newStatus, activities: newActivities, clientStatus: newClientStatus } = newPres || {};
+
+  // console.log(newActivities); // You can have multiple activities at once, e.g. Spotify+RuneLite
+  let ts = (new Date());
+  
+  if (oldActivities && oldActivities.length) {
+    const { name, details, state } = oldActivities[0];
+    oldActivities = `(${name}) ${details} - ${state}`;
+  }
+  if (newActivities && newActivities.length) {
+    const { name, details, state } = newActivities[0];
+    newActivities = `(${name}) ${details} - ${state}`;
+  }
+  oldClientStatusString = new Status(oldPres ? oldPres.clientStatus : {});
+  newClientStatusString = new Status(newPres ? newPres.clientStatus : {});
+
+  const whatChanged = oldClientStatusString !== newClientStatusString ? 'Status' : 'Activities';
+  console.log(`[${ts.toLocaleTimeString()}] Events.PresenceUpdate({ @${username}.${whatChanged} })`);
+
+  if (oldClientStatusString !== newClientStatusString) {
+    console.log(` - old: ${oldClientStatusString}\n - new: ${newClientStatusString}`);
+  }
+  if ((oldActivities && oldActivities.length || newActivities && newActivities.length) && (oldActivities !== newActivities)) {
+    console.log(` - old: ${oldActivities}\n - new: ${newActivities}`);
+  }
+});
+
+
+
+
+
+
+bennoBot.on(Events.GuildMemberUpdate, async function(oldMember, newMember) { console.log('Events.GuildMemberUpdate', oldMember, newMember); });
+
 bennoBot.login(clientSecret);
+
 // Handling ctrl-c to log the bot out
 const process = require('process'); // https://nodejs.org/api/process.html
 process.once('SIGINT', () => {
   console.log('\n ! process.once(SIGINT)\n -> bennoBot.destroy()');
   bennoBot.destroy();
+  // preventSleep.disable();
   process.exit(0);
 });
